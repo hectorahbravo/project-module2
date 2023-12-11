23bdd211d47141d422d866faad5c04bf10481707
Hector Hernandez <hectorahbravo@gmail.com>
2023-12-11 21:23:50 +0100

    updates

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
 # project-module2
+
+Blog de Cocina
+
+Entidades
+-Usuarios
+Username
+Email
+Password
+
+-Post de Recetas
+-Comentarios
+-Likes
+
+Vitas
+-Landing Page.
+-Registro
+-Login
+-Perfil
+-Muro
+
+-Sugerencias
+-Api de Recetas
---
+++ models/User.model.js
@@ -0,0 +1,37 @@
+const mongoose = required("mongoose");
+const bcrypt = required("becryptjs");
+
+const SALT_WORK_FACTOR = 10;
+const EMAIL_PATTERN =
+  /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+const UserSchema = mongoose.Schema({
+  username: {
+    type: String,
+    required: [true, "Username is required"],
+  },
+  email: {
+    type: String,
+    required: [true, "Email is required"],
+    unique: true,
+    match: [EMAIL_PATTERN, "Email is invalid"],
+  },
+  password: {
+    type: String,
+    required: [true, "Password is required"],
+    minLength: [8, "Password must be 8 characters or longer"],
+  },
+  activationToken: {
+    type: String,
+    default: () => {
+      return (
+        Math.random().toString(36).substring(2, 15) +
+        Math.random().toString(36).substring(2, 15)
+      );
+    },
+  },
+  isActive: {
+    type: Boolean,
+    default: false,
+  },
+});

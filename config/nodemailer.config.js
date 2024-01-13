c86dc03868e19b104df5faf0253484c6006faf8d
Pabloluis9 <140593315+Pabloluis9@users.noreply.github.com>
2024-01-13 14:34:55 +0100

    button-up

--- config/nodemailer.config.js
+++ config/nodemailer.config.js
@@ -236,7 +236,7 @@ module.exports.createEmailTemplate = (user) => {
     <!--[if mso]><style>.v-button {background: transparent !important;}</style><![endif]-->
   <div align="center">
     <!--[if mso]><v:roundrect xmlns:v="urn:schemas-microsoft-com:vml" xmlns:w="urn:schemas-microsoft-com:office:word" href="https://unlayer.com" style="height:37px; v-text-anchor:middle; width:290px;" arcsize="11%"  stroke="f" fillcolor="#469720"><w:anchorlock/><center style="color:#FFFFFF;"><![endif]-->
-      <a href="http://${process.env.HOST}/activate/${user.activationToken}" target="_blank" class="v-button" style="box-sizing: border-box;display: inline-block;text-decoration: none;-webkit-text-size-adjust: none;text-align: center;color: #FFFFFF; background-color: #469720; border-radius: 4px;-webkit-border-radius: 4px; -moz-border-radius: 4px; width:50%; max-width:100%; overflow-wrap: break-word; word-break: break-word; word-wrap:break-word; mso-border-alt: none;font-size: 14px;">
+      <a href="${process.env.HOST}/activate/${user.activationToken}" target="_blank" class="v-button" style="box-sizing: border-box;display: inline-block;text-decoration: none;-webkit-text-size-adjust: none;text-align: center;color: #FFFFFF; background-color: #469720; border-radius: 4px;-webkit-border-radius: 4px; -moz-border-radius: 4px; width:50%; max-width:100%; overflow-wrap: break-word; word-break: break-word; word-wrap:break-word; mso-border-alt: none;font-size: 14px;">
         <span style="display:block;padding:10px;line-height:120%;"><span style="line-height: 16.8px;">Activa tu cuenta!</span></span>
       </a>
       <!--[if mso]></center></v:roundrect><![endif]-->

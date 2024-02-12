fc2e66fb652fc1cc7e11ccf97e2c421e03c765b2
Hector Hernandez <hectorahbravo@gmail.com>
2024-02-12 15:45:45 +0100

    updates

--- config/db.config.js
+++ config/db.config.js
@@ -4,7 +4,9 @@ const DB_NAME = process.env.DB_NAME || "gustopolis";
 const MONGO_URI = process.env.MONGO_URI || "mongodb://127.0.0.1:27017";
 
 mongoose
-  .connect(MONGO_URI)
+  .connect(
+    "mongodb+srv://gustopolis:gustopolis.001@gustopolis.7ndgh88.mongodb.net/"
+  )
   .then(() => console.log(`Connected to db: ${DB_NAME}`))
   .catch((err) => console.error(`Error conecting to Mongo: ${err}`));
 

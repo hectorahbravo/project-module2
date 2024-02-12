41b592a51cea4d9af1dc341bf31b1d2180f9de9a
Hector Hernandez <hectorahbravo@gmail.com>
2024-02-12 18:58:21 +0100

    fix db.config

--- config/db.config.js
+++ config/db.config.js
@@ -4,7 +4,7 @@ const DB_NAME = process.env.DB_NAME || "gustopolis";
 const MONGO_URI = process.env.MONGO_URI || "mongodb://127.0.0.1:27017";
 
 mongoose
-  .connect(`${MONGO_URI}/${DB_NAME}`)
+  .connect(`${MONGO_URI}`)
   .then(() => console.log(`Connected to db: ${DB_NAME}`))
   .catch((err) => console.error(`Error conecting to Mongo: ${err}`));
 
